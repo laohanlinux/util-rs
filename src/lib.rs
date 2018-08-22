1c943699e37ba622198f1982dfd5855957464bc0
Rg <daimaldd@gmail.com>
2018-08-23 01:14:53 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
 mod number_bit;
+mod crypto;
+mod encoding;
+
+extern crate exonum_sodiumoxide as sodiumoxide;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
