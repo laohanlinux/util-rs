4cca5931ea5da40155dbd1f05a0753d2493121c2
Rg <daimaldd@gmail.com>
2019-05-04 02:55:28 +0800

    add fs util

--- ext-fs/src/lib.rs
+++ ext-fs/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(result_map_or_else)]
+
 use std::fs as stdfs;
 use std::path::Path;
 use std::io::{Read, Write};
@@ -27,10 +29,50 @@ pub fn create_write_all<P: AsRef<Path>>(path: P, mut buf: &[u8]) {
     }).unwrap()
 }
 
+pub fn exist_file<P: AsRef<Path>>(path: P) -> bool {
+    stdfs::metadata(path)
+        .map_or_else(|_| {
+            false
+        }, |meta| {
+            meta.is_file()
+        })
+}
+
+pub fn exist_dir<P: AsRef<Path>>(path: P) -> bool {
+    stdfs::metadata(path)
+        .map_or_else(|_| {
+            false
+        }, |meta| {
+            meta.is_dir()
+        })
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn exist_dir() {
+        let got = crate::exist_dir("/tmp");
+        assert!(got);
+
+        let got = crate::exist_dir("/tmp1");
+        assert!(!got);
+    }
+
+    #[test]
+    fn exist_file() {
+        use  crate::create_write_all;
+        create_write_all("/tmp/a", &vec![]);
+        let got = crate::exist_file("/tmp/a");
+        assert!(got);
+
+        let got = crate::exist_file("/tmp/a1");
+        assert!(!got);
+
+        crate::stdfs::remove_dir_all("/tmp/a");
     }
 }
