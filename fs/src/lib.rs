c7d2726dcfb9801c609fd8ebbe0947cd171988d4
Rg <daimaldd@gmail.com>
2019-05-03 12:55:45 +0800

    add fs util

---
+++ fs/src/lib.rs
@@ -0,0 +1,29 @@
+use std::fs as stdfs;
+use std::path::Path;
+use std::io::{Read, Write};
+
+pub fn read_to_string<P: AsRef<Path>>(path: P) -> String {
+    stdfs::File::open(path).map(|mut fp| {
+        let mut buffer = String::new();
+        fp.read_to_string(&mut buffer).unwrap();
+        fp.flush().unwrap();
+        buffer
+    }).unwrap()
+}
+
+pub fn read_to_end<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    stdfs::File::open(path).map(|mut fp| {
+        let mut buffer = Vec::new();
+        fp.read_to_end(&mut buffer).unwrap();
+        fp.flush().unwrap();
+        buffer
+    }).unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
