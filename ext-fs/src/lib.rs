8e8331f75175a038bc8b9117a90a0148ae8e0e80
Rg <daimaldd@gmail.com>
2021-01-11 18:45:36 +0800

    :tada:

--- ext-fs/src/lib.rs
+++ ext-fs/src/lib.rs
@@ -1,50 +1,46 @@
 #![feature(result_map_or_else)]
 
 use std::fs as stdfs;
-use std::path::Path;
 use std::io::{Read, Write};
+use std::path::Path;
 
 pub fn read_to_string<P: AsRef<Path>>(path: P) -> String {
-    stdfs::File::open(path).map(|mut fp| {
-        let mut buffer = String::new();
-        fp.read_to_string(&mut buffer).unwrap();
-        fp.flush().unwrap();
-        buffer
-    }).unwrap()
+    stdfs::File::open(path)
+        .map(|mut fp| {
+            let mut buffer = String::new();
+            fp.read_to_string(&mut buffer).unwrap();
+            fp.flush().unwrap();
+            buffer
+        })
+        .unwrap()
 }
 
 pub fn read_to_end<P: AsRef<Path>>(path: P) -> Vec<u8> {
-    stdfs::File::open(path).map(|mut fp| {
-        let mut buffer = Vec::new();
-        fp.read_to_end(&mut buffer).unwrap();
-        fp.flush().unwrap();
-        buffer
-    }).unwrap()
+    stdfs::File::open(path)
+        .map(|mut fp| {
+            let mut buffer = Vec::new();
+            fp.read_to_end(&mut buffer).unwrap();
+            fp.flush().unwrap();
+            buffer
+        })
+        .unwrap()
 }
 
 pub fn create_write_all<P: AsRef<Path>>(path: P, mut buf: &[u8]) {
-    stdfs::File::create(path).map(|mut fp| {
-        fp.write_all(&mut buf).unwrap();
-        fp.flush().unwrap();
-    }).unwrap()
+    stdfs::File::create(path)
+        .map(|mut fp| {
+            fp.write_all(&mut buf).unwrap();
+            fp.flush().unwrap();
+        })
+        .unwrap()
 }
 
 pub fn exist_file<P: AsRef<Path>>(path: P) -> bool {
-    stdfs::metadata(path)
-        .map_or_else(|_| {
-            false
-        }, |meta| {
-            meta.is_file()
-        })
+    stdfs::metadata(path).map_or_else(|_| false, |meta| meta.is_file())
 }
 
 pub fn exist_dir<P: AsRef<Path>>(path: P) -> bool {
-    stdfs::metadata(path)
-        .map_or_else(|_| {
-            false
-        }, |meta| {
-            meta.is_dir()
-        })
+    stdfs::metadata(path).map_or_else(|_| false, |meta| meta.is_dir())
 }
 
 #[cfg(test)]
@@ -65,7 +61,7 @@ mod tests {
 
     #[test]
     fn exist_file() {
-        use  crate::create_write_all;
+        use crate::create_write_all;
         create_write_all("/tmp/a", &vec![]);
         let got = crate::exist_file("/tmp/a");
         assert!(got);
